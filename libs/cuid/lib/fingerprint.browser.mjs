640c1e9ed44a9b68fe02c0d812f2dbaa18c996fb
Robert Munn <robertdmunn@gmail.com>
2020-04-05 03:35:21 -0600

    -- Moving custom cuid library into repo for now
    This is a temporary move, expect to see this custom cuid implementation (with native ESM support in the browser) moved into its own repo in the near future. This is simply being done for convenience.

---
+++ libs/cuid/lib/fingerprint.browser.mjs
@@ -0,0 +1,13 @@
+import {pad} from './pad.mjs';
+
+var env = typeof window === 'object' ? window : self;
+var globalCount = Object.keys(env).length;
+var mimeTypesLength = navigator.mimeTypes ? navigator.mimeTypes.length : 0;
+var clientId = pad((mimeTypesLength +
+  navigator.userAgent.length).toString(36) +
+  globalCount.toString(36), 4);
+
+
+export var fingerprint = function fingerprint () {
+ return clientId;
+};
